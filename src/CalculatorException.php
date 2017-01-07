40ed0d8fa0a6b3b5623d203088d55fb258591dce
Hoffmann András <hoffmann.andras@1nd.hu>
2017-01-07 09:16:13 +0100

    format

--- src/CalculatorException.php
+++ src/CalculatorException.php
@@ -17,7 +17,8 @@ class CalculatorException extends \LogicException
     public static function createInvalidSubmitHour(\DateTime $submitDate, int $minSubmitHour, int $maxSubmitHour): CalculatorException
     {
         return new static(
-            sprintf( "Invalid submit time [%s], it should be between %s and %s on workdays",
+            sprintf(
+                "Invalid submit time [%s], it should be between %s and %s on workdays",
                 $submitDate->format("Y-m-d H:i:s"),
                 $minSubmitHour,
                 $maxSubmitHour
